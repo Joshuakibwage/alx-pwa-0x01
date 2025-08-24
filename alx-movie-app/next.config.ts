f58c1556cfbd5aa6e9741a2e9adae7cc1150de44
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-24 21:25:21 +0300

    feat: Configure pwa in next.config.ts

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +7,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+export default withPWA({
+  ...nextConfig
+})
 
 
 // next.config.js
