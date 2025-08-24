57d40a859f1f358a3d994520eb100cfd03272a05
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-24 22:56:57 +0300

    build: production ready with pwa

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,16 +1,19 @@
+import { ReactNode } from "react";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
+type LayoutProps = {
+  children: ReactNode;
+};
 
-const Layout: React.FC = ({ children }) => {
-    return (
-        <>
-            <Header />
-                <div className="min-h-screen">{children}</div>
-            <Footer />
-        </>
-    )
-}
-
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <div className="min-h-screen">{children}</div>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,36 +1,23 @@
 import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
-
-
 const withPWA = withPWAInit({
-  dest: 'public'
-})
+  dest: "public", // required
+  register: true, // automatically register SW
+  // no skipWaiting here!
+});
 
-export default withPWA({
-  ...nextConfig
-})
-
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/images/**",
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default withPWA(nextConfig);
